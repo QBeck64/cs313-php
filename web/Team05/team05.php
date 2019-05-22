2deb9e73c4fd986ba833e875e0a2e1ad18b50741
QBeck64 <kad16001@byui.edu>
2019-05-22 09:10:56 -0700

    update heroku

--- web/Team05/team05.php
+++ web/Team05/team05.php
@@ -28,5 +28,15 @@
             die();
             }
     ?>
+    <?php
+    foreach ($db->query('SELECT * FROM scriptures') as $row)
+    {
+      echo 'book: ' . $row['book'];
+      echo ' chapter: ' . $row['chapter'];
+      echo 'verse:' . $row['verse'];
+      echo 'content' . $row['content'];
+      echo '<br/>';
+    }
+    ?>
     </body>
 </html>
