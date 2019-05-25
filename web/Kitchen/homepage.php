3b2a1a8ec1a73d566a43ebcb5d3aff93fa5b12d2
QBeck64 <kad16001@byui.edu>
2019-05-25 14:45:09 -0700

    test php

--- web/Kitchen/homepage.php
+++ web/Kitchen/homepage.php
@@ -3,3 +3,14 @@ require "dbConnect.php";
 $db = get_db();
 
 ?>
+
+<?php
+    foreach ($db->query('SELECT * FROM inventory') as $row)
+    {
+      echo "<p><span>" . $row['username_id'] . ' ';
+      echo $row['item_id'];
+      echo ':' . $row['expDate'] . ' - ' . "</span>";
+      echo '"' . $row['quantity'] . $row['entitylist_id'] . '"' . "</p>";
+      echo '<br/>';
+    }
+    ?>
