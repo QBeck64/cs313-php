151bf66d8e40adfa4e7658d5375cf2b2e1e3f484
QBeck64 <kad16001@byui.edu>
2019-06-01 14:32:40 -0700

    testing out user input fields

--- web/Kitchen/homepage.php
+++ web/Kitchen/homepage.php
@@ -33,7 +33,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   <a href="communityFridge.php">Community Fridge</a>
   <a href="pantry.php">Pantry</a>
   <a href="storageCloset.php">Storage Closet</a>
-  <a form action = "homepage.php" method = "post">
+  <a form action = "kitchen.php" method = "post">
         <label>UserName  :</label><input type = "text" name = "username"/>
         <label>Password  :</label><input type = "password" name = "password"/>
         <input type = "submit" value = " Submit "/>
