1b9c2dc1b31b183adb822687c741fc99ac2ad953
QBeck64 <kad16001@byui.edu>
2019-06-01 15:33:59 -0700

    testing

--- web/Kitchen/verifyUser.php
+++ web/Kitchen/verifyUser.php
@@ -5,9 +5,10 @@ $psw = htmlspecialchars($_POST['psw']);
 require('dbConnect.php');
 $db = get_db();
 
-$stmt = $db->prepare('SELECT * FROM username WHERE username_name=$username');
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-//$stmt->execute();
+$stmt = $db->prepare('SELECT * FROM username WHERE username_name=:user AND password=:psw');
+$stmt->bindvalue(':user', $user, PDO::PARAM_STR);
+$stmt->bindvalue(':psw', $psw, PDO::PARAM_STR);
+$stmt->execute();
 
 $new_page = "homepage.php";
 header("Location: $new_page");
