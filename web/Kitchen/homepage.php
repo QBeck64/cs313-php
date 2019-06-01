cee0a6690d6a8db80d902e9845eac5145cd075c5
QBeck64 <kad16001@byui.edu>
2019-06-01 14:42:28 -0700

    testing

--- web/Kitchen/homepage.php
+++ web/Kitchen/homepage.php
@@ -33,7 +33,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   <a href="communityFridge.php">Community Fridge</a>
   <a href="pantry.php">Pantry</a>
   <a href="storageCloset.php">Storage Closet</a>
-  <a form action = "kitchen.php" method = "post">
+  <a form action = "pantry.php" method = "post">
         <label>UserName  :</label><input type = "text" name = "username"/>
         <label>Password  :</label><input type = "password" name = "password"/>
         <input type = "submit" value = " Submit "/>
