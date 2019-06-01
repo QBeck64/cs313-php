24babec42165d4d7b30b4ee3f81c644970d295e6
QBeck64 <kad16001@byui.edu>
2019-06-01 15:15:17 -0700

    testing

--- web/Kitchen/verifyUser.php
+++ web/Kitchen/verifyUser.php
@@ -1,11 +1,5 @@
 <?php
-$user = htmlspecialchars($_POST['username']);
-$psw = htmlspecialchars($_POST['psw']);
-require('dbConnect.php');
-$db = get_db();
-$stmt = $db->prepare('SELECT * FROM username WHERE username_name=$username');
-$stmt->execute();
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
 $new_page = "homepage.php";
 header("Location: $new_page");
