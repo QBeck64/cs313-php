93f0c06be4e09fc02d54e938c80e4cf653a087e1
QBeck64 <kad16001@byui.edu>
2019-06-01 15:16:49 -0700

    testing

--- web/Kitchen/verifyUser.php
+++ web/Kitchen/verifyUser.php
@@ -5,7 +5,9 @@ $psw = htmlspecialchars($_POST['psw']);
 require('dbConnect.php');
 $db = get_db();
 
-
+$stmt = $db->prepare('SELECT * FROM username WHERE username_name=$username');
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $new_page = "homepage.php";
 header("Location: $new_page");
