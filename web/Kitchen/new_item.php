33db3b678bf97d83db36da7a4b60ced3b631ab39
QBeck64 <kad16001@byui.edu>
2019-06-01 17:56:34 -0700

    testing

--- web/Kitchen/new_item.php
+++ web/Kitchen/new_item.php
@@ -1,22 +1,6 @@
 <?php
 session_start();
-include("db_connect.php");
-$db = get_db();
 
-$userID = $_SESSION['userID'];
-
-include("db_functions.php");
-
-$data['newItem']    = filter_var($_POST['newItem'], FILTER_SANITIZE_STRING);
-$data['type'] = filter_var($_POST['type'], FILTER_SANITIZE_STRING);
-$data['newDescription']   = filter_var($_POST['newDescription'], FILTER_SANITIZE_STRING);
-$data['expdate'] =  date('Y-m-d', strtotime($_POST['expdate']));
-$data['quantity'] = htmlspecialchars($_POST['quantity']);
-$data['storage'] = filter_var($_POST['storage'], FILTER_SANITIZE_STRING);
-
-$itemID = insertItem($data);
-
-$newRow = addRow($data, $itemID, $userID);
 
 
 $new_page = "homepage.php";
